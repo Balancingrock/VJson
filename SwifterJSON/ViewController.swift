8b39def9db4dcdda4e6bf27410029aff3c5d775a
Rien <lugt@xs4all.nl>
2015-02-09 14:51:31 +0100

    Initial Commit

---
+++ SwifterJSON/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwifterJSON
+//
+//  Created by Marinus van der Lugt on 09/02/15.
+//  Copyright (c) 2015 Marinus van der Lugt. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+

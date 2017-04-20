8ba6a5086e62aabfa46398d0f44376404ceab262
Balancingrock <github@balancingrock.nl>
2017-04-20 11:34:16 +0200

    Added BRUtils

--- Package.swift
+++ Package.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
     name: "SwifterJSON",
     dependencies: [
-        .Package(url: "https://github.com/Balancingrock/Ascii", "1.0.0")
+        .Package(url: "https://github.com/Balancingrock/Ascii", "1.0.0"),
+        .Package(url: "../BRUtils", "0.3.0")
     ]
 )

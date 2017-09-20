b9bcc02e5aa44ff1b9bd601769a0f0f088bb129e
Balancingrock <github@balancingrock.nl>
2017-09-20 15:19:01 +0200

    Upgraded BRUtils to 0.9.0

--- Package.swift
+++ Package.swift
@@ -4,6 +4,6 @@ let package = Package(
     name: "VJson",
     dependencies: [
         .Package(url: "https://github.com/Balancingrock/Ascii", "1.0.0"),
-        .Package(url: "https://github.com/Balancingrock/BRUtils", "0.5.0")
+        .Package(url: "../../BRUtils", "0.9.0")
     ]
 )
---
+++ Sources/Bson.swift
@@ -0,0 +1,9 @@
+//
+//  Bson.swift
+//  VJson
+//
+//  Created by Marinus van der Lugt on 20/09/17.
+//
+//
+
+import Foundation
